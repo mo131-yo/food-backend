20d11ae1c5534fecc194d90789d52504abb8fce8
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-02-06 12:20:18 +0800

    as

--- src/utils/mail-utils.ts
+++ src/utils/mail-utils.ts
@@ -6,14 +6,18 @@ configDotenv();
 const { AUTH_EMAIL ,AUTH_PASS} = process.env;
 
 const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: AUTH_EMAIL,
-        pass: AUTH_PASS,
-    },
-    debug: true,
-    logger: true
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+ 
 
 
     export const verifyUserEmail = async (receiver: string, verifiLink: string) => {
